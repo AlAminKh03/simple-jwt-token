e325bd9b51aca2e66a5f6705d4d2970dccb25efb
AlAminKh03 <alaminkhan6203@gmail.com>
2022-05-10 11:24:08 +0600

    first commit

---
+++ src/Components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>tHIS IS ABOUT SECTION</h1>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/Components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <form>
+                <input type="email" name="" id="" />
+                <input type="password" name="" id="" />
+                <input type="submit" value="Login" />
+            </form>
+        </div>
+    );
+};
+
+export default Login;
